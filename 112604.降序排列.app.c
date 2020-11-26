794bdf52c49fced05c6fb8cd4ee055221046590e
123456789-abc123 <74294918+123456789-abc123@users.noreply.github.com>
2020-11-26 20:33:21 +0800

    Create 112604.降序排列.app.c

---
+++ 112604.降序排列.app.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void  main(){
+int  a,b,c,i;
+
+printf("输入abc三个数\n");
+scanf("%d%d%d",&a,&b,&c);
+if(a<b){i=a;a=b;b=i;}
+if(a<c){i=a;a=c;c=i;}
+if(b<c){i=b;b=c;c=i;}
+printf("%d,%d,%d\n",a,b,c);
+  
+
+
+}
