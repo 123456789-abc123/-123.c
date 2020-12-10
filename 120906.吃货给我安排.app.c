5ba15d20869e950acec6d4699d4ca7248f887bc0
123456789-abc123 <74294918+123456789-abc123@users.noreply.github.com>
2020-12-10 19:32:25 +0800

    Update 120906.吃货给我安排.app.c

--- 120906.吃货给我安排.app.c
+++ 120906.吃货给我安排.app.c
@@ -3,19 +3,19 @@
 void  main(){
 char  s1[50];
 scanf("%s",&s1);
-   if(strcmp(s1,"春节")){
+   if(!strcmp(s1,"春节")){
 printf("给我安排吃饺子\n");
 }
 
-else  if(strcmp(s1,"端午节")){
+else  if(!strcmp(s1,"端午节")){
 printf("给我安排吃粽子\n");
 }
 
-else  if(strcmp(s1,"中秋节")){
+else  if(!strcmp(s1,"中秋节")){
 printf("给我安排吃月饼\n");
 }
 
-else  if(strcmp(s1,"元宵节")){
+else  if(!strcmp(s1,"元宵节")){
 printf("给我安排吃元宵\n");
 }
 else{printf("什么都没有");}
